fe3cd43e2ccb715331cf2a4a6ae5cae5a927ca80
Jacob Cho <jacobshcho@gmail.com>
2018-04-04 20:48:36 -0700

    Initial Commit

---
+++ Email Validator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Email Validator
+//
+//  Created by Jacob Cho on 4/4/18.
+//  Copyright © 2018 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
